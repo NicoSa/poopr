1a9a6049c12c61a0d96d9577ea3597f3c2b5c90f
Roi Driscoll <roi.driscoll@gmail.com>
2015-06-07 12:46:09 +0100

    Adds project files

---
+++ Poopr/Poopr/Poopr.h
@@ -0,0 +1,19 @@
+//
+//  Poopr.h
+//  Poopr
+//
+//  Created by Roi Driscoll on 07/06/2015.
+//  Copyright (c) 2015 PairingBoyzz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Poopr.
+FOUNDATION_EXPORT double PooprVersionNumber;
+
+//! Project version string for Poopr.
+FOUNDATION_EXPORT const unsigned char PooprVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Poopr/PublicHeader.h>
+
+
